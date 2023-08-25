3cac31f7ca5afda024e3a6c7df06296fbc46963c
yardenPhy <yarden.s@PHYTECH.COM>
2023-08-25 15:49:52 +0300

    removed unnecessary commands

--- app-views/screen2.tsx
+++ app-views/screen2.tsx
@@ -7,62 +7,7 @@ interface Props {
   navigation: any;
 }
 
-const navigateMultipleScreen = (
-    navigation: any,
-    newRoutes: {name: string; params: {}}[],
-) =>  {
-  if (navigation) {
-    navigation.dispatch((state: any) => {
-      let newNavigationState;
-
-      const parentIndex = state.routes.findIndex(
-          (r: any) => r.name === APP_ROUTES.root,
-      );
-
-      if (parentIndex !== -1) {
-        const newAppRoute = Object.assign({}, state.routes[parentIndex], {
-          state: {
-            index: newRoutes.length - 1,
-            routeNames: Object.values(APP_ROUTES),
-            routes: newRoutes,
-            stale: true,
-            type: 'stack',
-          },
-        });
-        const cloneRoutes = state.routes.reduce((acc: any, r: any) => {
-          if (r.name === APP_ROUTES.root) {
-            acc.push(newAppRoute);
-          } else {
-            acc.push(r);
-          }
-          return acc;
-        }, []);
-
-        newNavigationState = {
-          ...state,
-          routes: cloneRoutes,
-        };
-      } else {
-        newNavigationState = {
-          ...state,
-          index: newRoutes.length - 1,
-          routes: newRoutes,
-          stale: true,
-          type: 'stack',
-        };
-      }
-
-      if (newNavigationState) {
-        return CommonActions.reset(newNavigationState);
-      }
-    });
-  }
-}
-
 export const Screen2: FunctionComponent<Props> = ({navigation}) => {
-
-
-
 
   const goToScreen3and4 = () => {
     // here is when it's going to crash
